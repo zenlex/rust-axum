802255cf34c80136313e39ad56252ecb29db7c12
zenlex <erichrkeil@gmail.com>
2023-05-07 18:47:24 -0500

    move hello response to handler

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,26 @@
 use std::net::SocketAddr;
 
-use axum::{response::Html, routing::get, Router};
+use axum::{
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route(
-        "/hello",
-        get(|| async { Html("Hello <strong>World!!</strong>") }),
-    );
+    let routes_hello = Router::new().route("/hello", get(handler_hello));
 
-    // region: ---start-server
+    // Start Server --->
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("--->> LISTENING on {addr}\n");
     axum::Server::bind(&addr)
         .serve(routes_hello.into_make_service())
         .await
         .unwrap();
-    // region: ---end-server
+}
+
+async fn handler_hello() -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello", "HANDLER");
+
+    Html("Hello <strong>World!!!</strong")
 }

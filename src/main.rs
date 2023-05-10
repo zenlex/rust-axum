5f8c9e6eae051fe2c9b32aef114e90a34e1caac2
zenlex <erichrkeil@gmail.com>
2023-05-09 19:26:13 -0500

    add static file service

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,57 @@
 use std::net::SocketAddr;
+use tower_http::services::ServeDir;
 
 use axum::{
+    extract::{Path, Query},
     response::{Html, IntoResponse},
     routing::get,
     Router,
 };
+use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route("/hello", get(handler_hello));
+    let routes_all = Router::new()
+        .merge(routes_hello())
+        .fallback_service(routes_static());
 
     // Start Server --->
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("--->> LISTENING on {addr}\n");
     axum::Server::bind(&addr)
-        .serve(routes_hello.into_make_service())
+        .serve(routes_all.into_make_service())
         .await
         .unwrap();
 }
 
-async fn handler_hello() -> impl IntoResponse {
-    println!("->> {:<12} - handler_hello", "HANDLER");
-
-    Html("Hello <strong>World!!!</strong")
+//#region Routes
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello/:name", get(handler_hello2))
 }
+
+fn routes_static() -> Router {
+    Router::new().nest_service("/", ServeDir::new("./"))
+}
+//#endregion
+
+//#region HelloHandler
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
+
+    let name = params.name.as_deref().unwrap_or("World");
+    Html(format!("Hello <strong>{name}</strong"))
+}
+
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello - {name:?}", "HANDLER");
+
+    Html(format!("Hello <strong>{name}</strong"))
+}
+//#endregion
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -4,7 +4,11 @@ use anyhow::Result;
 async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080")?;
 
-    hc.do_get("/hello").await?.print().await?;
+    hc.do_get("/hello?name=Erich").await?.print().await?;
+
+    hc.do_get("/hello/Erich").await?.print().await?;
+
+    hc.do_get("/src/main.rs").await?.print().await?;
 
     Ok(())
 }
